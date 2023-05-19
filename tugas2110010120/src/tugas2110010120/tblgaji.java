b5c2a840daaa092aed6ce4acaec040ed50e149bd
noveadahliani <134039826+noveadahliani@users.noreply.github.com>
2023-05-19 13:17:31 +0700

    Add files via upload

---
+++ tugas2110010120/src/tugas2110010120/tblgaji.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas2110010120;
+
+/**
+ *
+ * @author User
+ */
+public class tblgaji {
+     private String Nama;
+    private String NIPPOS;
+    private String Gaji_kotor;
+    private String Potongan;
+    private String Gaji_bersih;
+    
+    public tblgaji(){}
+    
+    public void setNama (String NOP){
+        this.Nama=Nama;
+    }
+    public String getNama(){
+        return this.Nama;
+    }
+    public void setNIPPOS (String NOP){
+        this.NIPPOS=NIPPOS;
+    }
+    public String getNIPPOS(){
+        return this.NIPPOS;
+    }
+    public void setGaji_kotor (String NOP){
+        this.Gaji_kotor=Gaji_kotor;
+    }
+    public String getGaji_kotor(){
+        return this.Gaji_kotor;
+    }
+    public void setPotongan (String NOP){
+        this.Potongan=Potongan;
+    }
+    public String getPotongan(){
+        return this.Potongan;
+    }
+    public void setGaji_bersih(String NOP){
+        this.Gaji_bersih=Gaji_bersih;
+    }
+    public String getGaji_bersih(){
+        return this.Gaji_bersih;
+    }
+    
+}
---
+++ tugas2110010120/src/tugas2110010120/tbllogin.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas2110010120;
+
+/**
+ *
+ * @author User
+ */
+public class tbllogin {
+    private String NIPPOS;
+    private String Password;
+    private String Login;
+    
+    public tbllogin(){}
+    
+    public void setNIPPOS (String NOP){
+        this.NIPPOS=NIPPOS;
+    }
+    public String getNIPPOS(){
+        return this.NIPPOS;
+    }
+    public void setPassword (String NOP){
+        this.Password=Password;
+    }
+    public String getPassword(){
+        return this.Password;
+    }
+    public void setLogin (String NOP){
+        this.Login=Login;
+    }
+    public String getLogin(){
+        return this.Login;
+    }
+}
